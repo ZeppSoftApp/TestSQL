f3f2011ed5ea262dec23a13f5555558ccda2ff27
Zepp <zeppsoftapp@gmail.com>
2024-07-31 17:40:09 +0300

    Small adjustment

--- Program.cs
+++ Program.cs
@@ -8,26 +8,18 @@ class Program
     {
         const string connectionString =
             "Server=localhost,1433\\Catalog=Test;Database=Test;User=SA;Password=Zaratustra2020;";
-        // "Data Source=localhost,1433;Initial Catalog=Test;User Id=sa; Password=Zaratustra2020;"
-        //  + "Integrated Security=true";
-
 
         using (SqlConnection con = new SqlConnection(connectionString))
         {
-           // con.Open();
-           // SqlCommand objSqlCommand = new SqlCommand("SELECT * FROM products", con);
             try
             {
                 HasRows(con);   
-                //var i = objSqlCommand.ExecuteNonQuery();dd111
             }
             catch (Exception ex) 
             { 
                 con.Close();
             }
         }
-
-
     }
     static void HasRows(SqlConnection connection)
     {
@@ -44,17 +36,10 @@ class Program
             {
                 while (reader.Read())
                 {
-                    //Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
-                    //  reader.GetString(1));
-                    //Console.WriteLine(reader.GetDecimal(0));
-
-                    //Console.WriteLine($"Price = {reader.GetSqlValue(0)?.ToString()}, Product_name = {reader.GetSqlValue(1)?.ToString()}");
-
                     var price = reader.GetSqlValue(0);
                     var productName = reader.GetSqlValue(1);
 
                     Console.WriteLine($"Price = {price?.ToString()}, Product_name = {productName?.ToString()}");
-
                 }
             }
             else
--- obj/Debug/net8.0/TestSQL.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestSQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestSQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6295d1ecd3b3b16e1dbb05a96fad28ea70dd7a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestSQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestSQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
